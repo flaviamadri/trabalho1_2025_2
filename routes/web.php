cbdf211d574818f5f9b0c1673ef2e4dc7350ddda
CoelhoVM <vitorya.m@aluno.ifsc.edu.br>
2025-09-13 15:43:57 -0300

    Add routes for Paciente and Consulta resources in web.php

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,23 @@ Route::get('/medico/edit/{id}', [MedicoController::class, 'edit'])->name('medico
 Route::put('/medico/update/{id}', [MedicoController::class, 'update'])->name('medico.update');
 Route::post('/medico/search', [MedicoController::class, 'search'])->name('medico.search');
 Route::delete('/medico/{id}', [MedicoController::class, 'destroy'])->name('medico.destroy');
+
+use App\Http\Controllers\PacienteController;
+
+Route::get('/paciente', [PacienteController::class, 'index'])->name('paciente.index');
+Route::get('/paciente/create', [PacienteController::class, 'create'])->name('paciente.create');
+Route::post('/paciente', [PacienteController::class, 'store'])->name('paciente.store');
+Route::get('/paciente/edit/{id}', [PacienteController::class, 'edit'])->name('paciente.edit');
+Route::put('/paciente/update/{id}', [PacienteController::class, 'update'])->name('paciente.update');
+Route::post('/paciente/search', [PacienteController::class, 'search'])->name('paciente.search');
+Route::delete('/paciente/{id}', [PacienteController::class, 'destroy'])->name('paciente.destroy');
+
+use App\Http\Controllers\ConsultaController;
+
+Route::get('/consulta', [ConsultaController::class, 'index'])->name('consulta.index');
+Route::get('/consulta/create', [ConsultaController::class, 'create'])->name('consulta.create');
+Route::post('/consulta', [ConsultaController::class, 'store'])->name('consulta.store');
+Route::get('/consulta/edit/{id}', [ConsultaController::class, 'edit'])->name('consulta.edit');
+Route::put('/consulta/update/{id}', [ConsultaController::class, 'update'])->name('consulta.update');
+Route::post('/consulta/search', [ConsultaController::class, 'search'])->name('consulta.search');
+Route::delete('/consulta/{id}', [ConsultaController::class, 'destroy'])->name('consulta.destroy');
